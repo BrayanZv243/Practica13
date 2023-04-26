f4e9c2dc8c6fc26553abbcc093eb55e1cfebc04e
Brayan Zavala <zavala_243@hotmail.com>
2023-04-26 12:22:05 -0700

    full project

---
+++ app/src/main/java/mx/itson/edu/practica13/MainActivity.kt
@@ -0,0 +1,27 @@
+package mx.itson.edu.practica13
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import mx.itson.edu.practica13.databinding.ActivityMainBinding
+import mx.itson.edu.practica13.databinding.ActivitySignInBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+    }
+}
